f6ea5f8b57f4c576d42df8e8cc370fd2650a9f5e
0racl3 vCreativeIX <macgia1999@gmail.com>
2020-02-29 00:00:59 +0700

    initial commit

--- Bai2.php
+++ Bai2.php
@@ -1,21 +1,26 @@
 <html>
 	<body>
+
 		<?php 
 		$Number=$_POST["Number"];
 		$Cost=$_POST["Cost"];
-		 ?>
-		 <?php 
-		function Tongtien($Num,$Cost)
-		 {
-		 	if ($Num <=1; $Cost <=1) {
-		 		return 1;
-		 	}
-		 	else{
-		 		return $Num*$Cost*1.1;
-		 	}
-		 }
-		 $Ketqua=Tongtien($Number)
-		 echo "Tổng số tiền" .$Number."là:".$Ketqua;
-		  ?>
+		?>
+
+		<?php 
+			function Tongtien($Num,$Cost)
+			{
+				if ($Num <=1 && $Cost <=1) {
+					return 1;
+				}else {
+					return $Num*$Cost*1.1;
+				}
+			}
+			function HienthiM($value) {
+				if ($value<0) return "-".HienthiM(-$value);
+				return  number_format($value, 0) .' VNĐ';
+			}
+			$Ketqua=Tongtien($Number,$Cost);
+			echo "Tổng số tiền ". HienthiM($Number) ." là: ". HienthiM($Ketqua);
+		?>
 	</body>
 </html>
--- Bai3.php
+++ Bai3.php
@@ -1,12 +1,12 @@
 <html>
 	<body>
 		
-		Name <?php echo $_POST["Name"];?><br>
-		Address <?php echo $_POST["Address"]; ?> <br>
-		GT <?php echo $_POST["Gioi tinh"]; ?> <br>
-		Hometown <?php echo $_POST["Home town"]; ?> <br>
-		Phersonalnotes <?php echo $_POST["Phersonal notes"]; ?> <br>
-		Picture <?php echo $_POST["Picture"]; ?> <br>
+		Name: <?php echo $_POST["Name"];?><br>
+		Address: <?php echo $_POST["Address"];?> <br>
+		GT: <?php echo $_POST["Sex"];?> <br>
+		Hometown: <?php echo $_POST["Hometown"];?> <br>
+		Phersonalnotes: <?php echo $_POST["Phersonalnotes"]; ?> <br>
+		Picture: <?php echo $_POST["Picture"]; ?> <br>
 		 
 	</body>
 </html>
